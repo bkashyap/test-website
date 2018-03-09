36458798ead7c5bf7ffd91864586e84eaa159ef2
Bharath Kashyap <bharathkashyap@Bharaths-MacBook-Pro.local>
2018-03-09 14:20:27 -0800

    this is a test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Yo wassssup dude');
+  response.end('Yo wassssup dude, this is just a test');
 };
 var www = http.createServer(handleRequest);
 www.listen(8081);
